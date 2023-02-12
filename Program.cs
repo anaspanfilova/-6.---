81b1d6f1fb22661081cee1cb60fbd62a2d46fc76
Анастасия <understooooood@mail.ru>
2023-02-12 21:38:18 +0300

    внесены корректировки

--- Program.cs
+++ Program.cs
@@ -18,38 +18,9 @@
 // [“Russia”, “Denmark”, “Kazan”] → []
 
 //РЕШЕНИЕ 1 (с использованием двух массивов)
+
 Console.Write("Сколько элементов Вы хотите ввести? ");
 int size = Convert.ToInt32(Console.ReadLine());
-
 string[] arrayStrings = new string[size];
-for    (int i=0; i
-{
-    Console.WriteLine($"Введите {i+1}-й элемент: ");
-    string element = Convert.ToString(Console.ReadLine());
-    arrayStrings[i] = element;
-}
-
-string[] arrayFinal = new string[size];
 int len = 3;
 int pos = 0;
-
-for    (int j=0; j  
-{
-    if (arrayStrings[j].Length <= len)
-    {
-        arrayFinal[pos] = arrayStrings[j];
-        pos++;
-    }
-}
-
-Console.WriteLine();
-PrintArray(arrayFinal);
-
-void PrintArray(string[] array)
-{
-    for    (int i=0;   i<array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
